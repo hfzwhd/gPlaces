5fb1e00090fee2209bcbbea2b3320b3f12ee6a99
hfzwhd <hafizwahid.e@gmail.com>
2017-05-24 19:47:57 +0800

    init commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'gPlaces' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for gPlaces
+  pod 'DCKit', :git => 'https://github.com/agordeev/DCKit.git'
+  pod 'ChameleonFramework/Swift', :git => 'https://github.com/ViccAlexander/Chameleon.git'
+  pod 'GoogleMaps'
+  pod 'GooglePlaces'
+  pod 'GooglePlacePicker'
+
+end
---
+++ gPlaces/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  gPlaces
+//
+//  Created by Hafiz Wahid on 24/05/2017.
+//  Copyright © 2017 hw. All rights reserved.
+//
+
+import UIKit
+import GooglePlaces
+import GooglePlacePicker
+
+class ViewController: UIViewController
+{
+
+    @IBOutlet weak var placeNameLabel: UILabel!
+    @IBOutlet weak var placeAddressLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func inputClick(_ sender: Any)
+    {
+        let acController = GMSAutocompleteViewController()
+        acController.delegate = self
+        present(acController, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func mapClick(_ sender: Any)
+    {
+        
+        let config = GMSPlacePickerConfig(viewport: nil)
+        let placePicker = GMSPlacePicker(config: config)
+        
+        placePicker.pickPlace(callback: { (place, error) -> Void in
+            if let error = error
+            {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let place = place else
+            {
+                print("No place selected")
+                return
+            }
+            
+            print("Place name \(place.name)")
+            self.placeNameLabel.text = place.name
+            print("Place address \(place.formattedAddress)")
+            self.placeAddressLabel.text = place.formattedAddress
+            print("Place attributions \(place.attributions)")
+        })
+        
+    }
+    
+    
+
+
+}
+
+extension ViewController: GMSAutocompleteViewControllerDelegate
+{
+    
+    // handle user selection
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace)
+    {
+        print("Place name: \(place.name)")
+        self.placeNameLabel.text = place.name
+        print("Place address: \(place.formattedAddress)")
+        self.placeAddressLabel.text = place.formattedAddress
+        print("Place attributions: \(place.attributions)")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error)
+    {
+        // TODO: handle the error.
+        print("Error: \(error)")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // user cancelled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController)
+    {
+        print("Autocomplete was cancelled.")
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
